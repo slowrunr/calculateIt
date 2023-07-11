4510d94d11927c887b4e182381edda94176c11b9
slowrunr <mungaslov.nft@gmail.com>
2023-07-11 23:59:49 +0900

    init

---
+++ math.js
@@ -0,0 +1,17 @@
+//basic calculator functions
+
+function sum(a, b) {
+  return a + b;
+}
+
+function subtract(a, b) {
+  return a - b;
+}
+
+function multiply(a, b) {
+  return a * b;
+}
+
+function divide(a, b) {
+  return a / b;
+}
