9d9c3a6e5879e86d3d8ae07c0a2ea073cbff5334
slowrunr <mungaslov.nft@gmail.com>
2023-07-11 23:59:40 +0900

    simple calculator

---
+++ index.js
@@ -0,0 +1,6 @@
+const inputAnode = document.querySelector(".js-input-a");
+const inputBnode = document.querySelector(".js-input-b");
+const selectOperationnode = document.querySelector(".js-input-b");
+js - select - operation;
+const btnResultnode = document.querySelector(".js-btn-result");
+const outputnode = document.querySelector(".js-output");
